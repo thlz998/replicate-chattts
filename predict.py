b08528529405c947ba75942f3bbf11bf0350f96c
殷迦南 <thlz998@gmail.com>
2024-06-02 19:27:33 +0800

    feat/update setup

--- predict.py
+++ predict.py
@@ -16,6 +16,7 @@ import time
 import soundfile as sf
 from modelscope import snapshot_download
 
+
 # Constants
 class Predictor(BasePredictor):
     def setup(self):
@@ -24,7 +25,20 @@ class Predictor(BasePredictor):
         CHATTTS_DIR = snapshot_download('pzc163/chatTTS', cache_dir=MODEL_DIR)
         self.chat = ChatTTS.Chat()
         self.chat.load_models(source="local", local_path=CHATTTS_DIR)
-
+        # 启动后初始化
+        self.chat.infer(
+            [t for t in "hello".split("\n") if t.strip()],
+            use_decoder=True,
+            skip_refine_text=bool(0),
+            params_infer_code={
+                'spk_emb':  self.chat.sample_random_speaker(),
+                'temperature': 0.3,
+                'top_P': 0.7,
+                'top_K': 20
+            },
+            params_refine_text={'prompt': ""},
+            do_text_normalization=False
+        )
     @torch.inference_mode()
     def predict(self,
                 text: str = Input(description="Text to be synthesized", default="Hello world!"),
